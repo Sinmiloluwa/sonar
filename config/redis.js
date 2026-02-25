5bae7954f0af04e465401a46d6afe2938dc33ee7
Sinmiloluwa <mofeoluwae@gmail.com>
2026-02-26 00:46:45 +0100

    allow for redis url

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,13 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-    password: process.env.REDIS_PASSWORD,
-    maxRetriesPerRequest: null
-});
+const redis = process.env.REDIS_URL
+    ? new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+    : new Redis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD,
+        maxRetriesPerRequest: null,
+    });
 
 redis.on('connect', () => {
     console.log('Connected to Redis');
